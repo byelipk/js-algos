8ca39bb368a6965667c1a6f58833974350d7cb7c
byelipk <byelipk@gmail.com>
2017-05-31 16:41:34 -0400

    Recursion strategy

--- recursion.js
+++ recursion.js
@@ -1,3 +1,7 @@
+// 1. Identify base cases
+// 2. Identify recursive cases
+// 3. Return where appropriate
+// 4. Write procedures for each case that bring us closer to the base case(s)
 function sayHi(n) {
   if (n === 0) {
     return "Done!"
