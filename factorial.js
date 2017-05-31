ec3720695a11b4530fd4407f4d5aa317fc11392e
byelipk <byelipk@gmail.com>
2017-05-31 09:21:37 -0400

    Implement recursion with 1 arg

--- factorial.js
+++ factorial.js
@@ -20,9 +20,19 @@ function computeFactorialRec(result, num) {
   }
 }
 
+function computeFactorialRec2(num) {
+  if (num === 1) {
+    return 1;
+  }
+  else {
+    return num * computeFactorialRec2(num - 1);
+  }
+}
+
 // Five factorial => 5! => 5*4*3*2*1
 console.log(computeFactorialIter(5));
 console.log(computeFactorialRec(1, 5));
 
 // Six factorial => 6! => 6*5*4*3*2*1
 console.log(computeFactorialRec(1, 6));
+console.log(computeFactorialRec2(6));
