c43459013453fdaa15a6025aa8b45fc479a2d8b7
byelipk <byelipk@gmail.com>
2017-05-31 18:04:46 -0400

    Implement a queue with two stacks

---
+++ stack-queue.js
@@ -0,0 +1,66 @@
+const Stack = require("./stack.js").Stack;
+
+// QUEUE
+//
+// Abstract data type
+// FIFO - First in, first out
+// Collection of elements with enqueue and dequeue operations.
+// Note that there is a natural order. Elements are removed in the reverse
+// order of their addition.
+//
+//
+//
+// *** Operations:
+//
+// stackQueue.enqueue(value)
+// => count of queue
+// add value to collection
+//
+// stackQueue.dequeue()
+// => oldest element added collection
+// Remove item so that it is no longer in collection
+//
+// stackQueue.peek()
+// => oldest element added collection
+// Similiar to dequeue, but do not remove element from collection
+//
+// stackQueue.count()
+// => number of elements in queue
+//
+
+class StackQueue {
+
+  constructor(capacity) {
+    this._stackIn  = new Stack(capacity);
+    this._stackOut = new Stack(capacity);
+  }
+
+  enqueue(value) {
+    return this._stackIn.push(value);
+  }
+
+  dequeue() {
+    if (this._stackOut.count() === 0) { this._musicalChairs(); }
+    return this._stackOut.pop();
+  }
+
+  peek() {
+    return this._stackIn.peek();
+  }
+
+  count() {
+    return this._stackIn.count();
+  }
+
+  // NOTE
+  // For stack-queue to work we need a mechanism for transfering
+  // all the elements from the input stack into the output stack
+  // in the correct order.
+  _musicalChairs() {
+    while (this._stackIn.count() > 0) {
+      this._stackOut.push(this._stackIn.pop());
+    }
+  }
+}
+
+module.exports = StackQueue;
---
+++ tests/stack-queue-test.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+import StackQueue from '../stack-queue.js';
+
+test('it works', t => {
+  t.plan(1);
+  t.pass();
+});
+
+test('enqueue returns count of input stack', t => {
+  t.plan(1);
+
+  const q = new StackQueue();
+
+  t.truthy(q.enqueue(100) === 1);
+});
+
+test('dequeue returns first item in queue', t => {
+  t.plan(1);
+
+  const q = new StackQueue();
+
+  q.enqueue(10);
+  q.enqueue(20);
+  q.enqueue(30);
+
+  t.truthy(q.dequeue() === 10);
+});
