43f47cf779e1a85458f442db19a4a1e67a9dd1c9
byelipk <byelipk@gmail.com>
2017-06-01 12:18:25 -0400

    Upgrade bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,13 +1,26 @@
 class BubbleSort {
   run(list) {
-    for (var i = 0; i < list.length; i++) {
-      for (var j = 0; j < list.length; j++) {
-        if (list[j] > list[i]) {
+    // If the list only contains 1 element
+    // then we can return it as is.
+    if (list.length === 1) {
+      return list;
+    }
+
+    for (let i = 0; i < (list.length - 1); i++) {
+      let swapped = false;
+      
+      for (let j = 0; j < (list.length - 1 - i); j++) {
+        if (list[j + 1] < list[j]) {
           let temp = list[j];
-          list[j]  = list[i];
-          list[i]  = temp;
+          list[j] = list[j+1];
+          list[j+1] = temp;
+          swapped = true;
         }
       }
+
+      // If the sort makes no exchanges then
+      // the algorithm can be stopped
+      if (!swapped) break;
     }
 
     return list;
--- tests/bubble-sort-test.js
+++ tests/bubble-sort-test.js
@@ -8,3 +8,12 @@ test('it works', t => {
 
   t.deepEqual(sorter.run([2,3,1]), [1,2,3]);
 });
+
+
+test('it works on sorted array', t => {
+  t.plan(1);
+
+  const sorter = new BubbleSort();
+
+  t.deepEqual(sorter.run([1,2,3]), [1,2,3]);
+});
